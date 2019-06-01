e68d17f88a70f9e655eb9e314a403ef8501133e5
mikanbox <popoloas@yahoo.co.jp>
2019-06-02 08:31:04 +0900

    bug fix

--- main.py
+++ main.py
@@ -32,17 +32,18 @@ def hello():
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+    # signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    # # get request body as text
+    # body = request.get_data(as_text=True)
+    # app.logger.info("Request body: " + body)
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+    # # handle webhook body
+    # try:
+    #     handler.handle(body, signature)
+    # except InvalidSignatureError:
+    #     abort(400)
+    
     return 'OK'
 
 
